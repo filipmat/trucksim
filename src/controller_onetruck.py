f0417b496365b5ae16cad52462d5df17a986914d
Filip <mattsson.filip@gmail.com>
2018-02-09 14:31:42 +0100

    Controller code cleanup.

--- src/controller_onetruck.py
+++ src/controller_onetruck.py
@@ -11,38 +11,26 @@ import path
 import frenetpid
 
 class Controller():
-    """Class for subscribing to topic mocap data, calculate control input and
-    send commands to the truck. """
-    def __init__(self, node_name, position_topic_type, position_topic_name,
-        control_topic_type, control_topic_name,
-        v = 0, k_p = 0, k_i = 0, k_d = 0, vehicle_id = 2):
+    """Class for subscribing to vehicle positions, calculate control input, and
+    send commands to the vehicle. """
+    def __init__(self, position_topic_type, position_topic_name,
+        control_topic_type, control_topic_name, vehicle_id,
+        node_name = 'controller', v = 1, k_p = 0, k_i = 0, k_d = 0):
 
-        # List of strings used by the GUI to see which values it can adjust.
-        self.adjustables = ['k_p', 'k_i', 'k_d', 'v']
+        self.v = v                      # Desired velocity of the vehicle.
 
-        # Velocity of the truck and PID parameters.
-        self.v = v
+        self.vehicle_id = vehicle_id    # ID of the vehicle.
 
-        self.stop_angle = 0
+        self.running = False            # If controller is running or not.
 
-        self.vehicle_id = vehicle_id
-
-        # Radii and center for reference path ellipse.
-        self.xr = 0
-        self.yr = 0
-        self.xc = 0
-        self.yc = 0
-
-        self.running = False    # Controlling if controller is running or not.
-
-        # Setup node.
+        # Setup ROS node.
         rospy.init_node(node_name, anonymous = True)
 
         # Subscriber for vehicle positions.
         rospy.Subscriber(
             position_topic_name, position_topic_type, self._callback)
 
-        # Publisher for controlling vehicles.
+        # Publisher for controlling vehicle.
         self.pub = rospy.Publisher(control_topic_name, control_topic_type,
             queue_size = 1)
 
@@ -57,37 +45,39 @@ class Controller():
 
     def _callback(self, data):
         """Called when the subscriber receives data. """
-
+        # Retrieve data.
+        vehicle_id = data.id
         x = data.x
         y = data.y
-        yaw = data.theta
-        vel = 1
+        theta = data.theta
+        vel = 1             # Placeholder for actual velocity of vehicle.
 
-        if data.id == self.vehicle_id:
-            self._control(x, y, yaw, vel)
+        # Perform control if the vehicle ID is the same as in the controller.
+        if vehicle_id == self.vehicle_id:
+            self._control(x, y, theta, vel)
 
 
-    def _control(self, x, y, yaw, vel):
+    def _control(self, x, y, theta, vel):
         """Perform control actions from received data. Sends new values to
         truck. """
         if self.running:
+            # Get control input.
+            omega = self.frenet.get_omega(x, y, theta, vel)
 
-            omega = self.frenet.get_omega(x, y, yaw, vel)
+            # Publish control commands to the topic.
+            self.pub.publish(self.vehicle_id, self.v, omega)
 
-            self.pub.publish(self.vehicle_id, 1, omega)
-
-            self.stop_angle = 0
-
-            print(self.frenet.get_y_error())
+            # Display control error.
+            print('Control error: {:.3f}'.format(self.frenet.get_y_error()))
 
 
     def stop(self):
         """Stops/pauses the controller. """
-        t = 0.05
+        t = 0.5
 
-        self.pub.publish(self.vehicle_id, 0, self.stop_angle)
+        self.pub.publish(self.vehicle_id, 0, 0)
         time.sleep(t)
-        self.pub.publish(self.vehicle_id, 0, self.stop_angle)
+        self.pub.publish(self.vehicle_id, 0, 0)
 
         if self.running:
             self.running = False
@@ -104,70 +94,40 @@ class Controller():
             print('Controller started.')
 
 
-    def set_adjustables(self, values):
-        """Used by the GUI to set the adjustable values. values is a list with
-        the same size as the list returned by get_adjustables(). The values
-        should here be treated in the same order as specified in that list. """
-        try:
-            k_p = float(values[0])
-            k_i = float(values[1])
-            k_d = float(values[2])
-            v = float(values[3])
-
-        except:
-            print('\nInvalid control parameters entered.')
-            return
-
-        self.frenet.set_pid(k_p, k_i, k_d)
-        self.v = v
-        self.v_pwm = self.translator.get_speed(self.v)
-
-        print('\nControl parameter changes applied.')
-
-
-    def get_adjustables(self):
-        """Used by the GUI to get the values that are adjustable.
-        Returns two lists. The first list is a list of the names/descriptors
-        of the adjustable parameters. The second is the current values of those
-        parameters. """
-        k_p, k_i, k_d = self.frenet.get_pid()
-
-        return self.adjustables, [k_p, k_i, k_d, self.v]
-
-
     def set_reference_path(self, radius, center = [0, 0], pts = 400):
         """Sets a new reference ellipse path. """
-        if isinstance(radius, list):
-            if len(radius) > 1:
-                self.xr = radius[0]
-                self.yr = radius[1]
-            else:
-                self.xr = radius[0]
-                self.yr = radius[0]
-        else:
-            self.xr = radius
-            self.yr = radius
-
-        self.xc = center[0]
-        self.yc = center[1]
-        self.pt.gen_circle_path([self.xr, self.yr], pts, [self.xc, self.yc])
+        self.pt.gen_circle_path(radius, pts, center)
 
 
     def run(self):
-        """Runs the controller. Needs to be called if not using the GUI. """
+        """Runs the controller. """
         self.start()
         rospy.spin()
         self.stop()
 
 
+    def set_pid(self, kp, ki, kd):
+        """Sets the PID parameters. """
+        self.frenet.set_pid(kp, ki, kd)
+
+
+    def set_speed(self, v):
+        """Sets the vehicle speed. """
+        self.v = v
+
+
 def main(args):
+    # ID of the vehicle.
     vehicle_id = '/' + args[1]
 
-    # Information for controller subscriber.
+    # Name of ROS node.
     node_name = 'controller'
+
+    # Topic information for subscribing to truck positions.
     position_topic_name = 'vehicle_position'
     position_topic_type = vehicleposition
 
+    # Topic information for publishing vehicle commands.
     control_topic_name = 'vehicle_control'
     control_topic_type = vehiclecontrol
 
@@ -176,23 +136,24 @@ def main(args):
     y_radius = 1.2
     center = [0.3, -1.3]
 
-    # Controller tuning variables.
-    v = 0.89             # Velocity used by translator model.
+    # Constant velocity of vehicle.
+    v = 1
 
+    # PID parameters.
     k_p = 0.5
     k_i = -0.02
     k_d = 3
 
     # Initialize controller.
     controller = Controller(
-        node_name, position_topic_type, position_topic_name,
-        control_topic_type, control_topic_name,
-        v = v, k_p = k_p, k_i = k_i, k_d = k_d,
-        vehicle_id = vehicle_id)
+        position_topic_type, position_topic_name, control_topic_type,
+        control_topic_name, vehicle_id, node_name,
+        v = v, k_p = k_p, k_i = k_i, k_d = k_d)
 
     # Set reference path.
     controller.set_reference_path([x_radius, y_radius], center)
 
+    # Start controller.
     controller.run()
 
 
